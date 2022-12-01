8c757a73e2a9155035bd50b7fed7d3b659835784
xongjaemin <woals99@gmail.com>
2022-12-01 20:51:01 +0900

    feat: 불꽃에 소리 fix: 뉴욕맵 핫핑크 머테리얼 수정

---
+++ Assets/Scripts/ParticleSoundSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class FireworksParticleSoundSystem : MonoBehaviour
+{
+
+    AudioSource audioSoure;
+
+    void Start()
+    {
+        audioSoure = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        while (true)
+        {
+            audioSoure.Play();
+        }
+    }
+
+    private void ApplyExplosionForceInArea(Vector3 position, bool applyPhysicsForce = true, bool applyShakeEffect = true)
+    {
+        audioSoure.Play();
+    }
+
+    private class ParticleDelta
+    {
+        public IList<ParticleSystem.Particle> Added { get; set; } = new List<ParticleSystem.Particle>();
+        public IList<ParticleSystem.Particle> Removed { get; set; } = new List<ParticleSystem.Particle>();
+    }
+}
